e294d896a6fa193814846aef23dcf232ed474625
SrHoim <1018823281@qq.com>
2018-07-24 08:24:43 +0800

    js1

--- js/index.js
+++ js/index.js
@@ -1,26 +1,6 @@
-/*window.addEventListener('load', function(){
-		var swiper = new Swiper('.swiper-container', {
-		      centeredSlides: true,
-		      loop: true,
-		      hashNavigation: {
-		      watchState: true,
-		      },
-		      autoplay: {
-		        delay: 2500,
-		        disableOnInteraction: false,
-		      },
-		      pagination: {
-		        el: '.swiper-pagination',
-		        clickable: true,
-		      },
-		      navigation: {
-		        nextEl: '.swiper-button-next',
-		        prevEl: '.swiper-button-prev',
-		      },
-	    });
-    })*/
-
     window.onload=function(){
+
+    	//侧导航
     	let li=document.getElementsByClassName('li');
     	console.log(li);
     	let liBox=document.getElementsByClassName('liBox');
@@ -34,5 +14,55 @@
     			liBox[i].style.display='none';
     			li[i].className='li';
     		}
+    	}
+
+    	//轮播图
+    	let tu_banner=document.getElementsByClassName('tu_banner')[0];
+    	let lis=tu_banner.getElementsByTagName('li');
+    	let img=tu_banner.getElementsByTagName('img');
+    	let leftGo=document.getElementsByClassName('banner-left')[0];
+    	let rightGo=document.getElementsByClassName('banner-right')[0];
+        let lidian=tu_banner.getElementsByClassName('banner-nav-ls');
+
+    	let num=0;
+    	let t=setInterval(move,2000);
+    	function move(){
+    		num++;
+    		if(num==lis.length){
+    			num=0;
+    		}
+    		for(let i=0;i<lis.length;i++){
+    			img[i].style.zIndex=5;
+                lidian[i].style.background='rgba(255, 255, 255, 0.7)';
+    		}
+    		img[num].style.zIndex=10;
+            lidian[num].style.background='#f90';
+    	}
+
+    	tu_banner.onmouseenter=function(){
+    		clearInterval(t);
+    	}
+    	tu_banner.onmouseleave=function(){
+    		t=setInterval(move,2000);
+    	}
+
+    	function move1(){
+    		num--;
+    		if(num<0){
+    			num=lis.length-1;
+    		}
+    		for(let i=0;i<lis.length;i++){
+    			img[i].style.zIndex=5;
+                lidian[i].style.background='rgba(255, 255, 255, 0.7)';
+    		}
+    		img[num].style.zIndex=10;
+            lidian[num].style.background='#f90';
+    	}
+
+    	leftGo.onclick=function(){
+    		move1();
+    	}
+    	rightGo.onclick=function(){
+    		move();
     	}
     }
